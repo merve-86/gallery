2b15b1c01e91c139a6d1b18500f7d89fc5522ba8
merve <merveaydin86@hotmail.com>
2024-03-20 11:15:30 +0300

    commit

---
+++ src/components/picture.jsx
@@ -0,0 +1,23 @@
+import data from "../helper/data"
+import "../styles/style.css"
+
+
+function Pictures() {
+    return (
+        <>
+        <h1>Image Gallery</h1>
+        <div className="pictures">
+            {data.map((item, index) => (
+                <div key={index} className="picture">
+                    <div className="imageContainer">
+                        <img src={item.src.large} alt={item.photographer} />
+                    </div>
+                    <div className="info">{item.photographer}</div>
+                </div>
+            ))}
+        </div>
+        </>
+    )
+}
+
+export default Pictures
